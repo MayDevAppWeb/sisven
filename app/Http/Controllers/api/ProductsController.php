28755ad7f963b7f8b2dfb70f9ce2e3e9752fcaeb
May Gaviria <may.dev.app.web@gmail.com>
2023-05-17 02:06:14 -0500

    Pay_mode-completo controlador y routes

---
+++ app/Http/Controllers/api/ProductsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Sisven;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\api\CategoriesController;
 use App\Http\Controllers\api\CustomersController;
+use App\Http\Controllers\api\PayModeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -39,17 +40,35 @@ Route::delete('/customers/{id}', [CustomersController::class, 'destroy'])
 Route::get('/categories', [CategoriesController::class, 'index'])
 ->name('categories.index');
 // store categories
-Route::post('/categories' , [categoriesController::class, 'store'])
+Route::post('/categories' , [CategoriesController::class, 'store'])
 ->name('custoemers.store');
 // show categories
-Route::get('/categories/{id}', [categoriesController::class, 'show'])
+Route::get('/categories/{id}', [CategoriesController::class, 'show'])
 ->name('categories.show');
 // update categories
-Route::put('/categories/{id}', [categoriesController::class, 'update'])
+Route::put('/categories/{id}', [CategoriesController::class, 'update'])
 ->name('categories.update');
 // destroy categories
-Route::delete('/categories/{id}', [categoriesController::class, 'destroy'])
+Route::delete('/categories/{id}', [CategoriesController::class, 'destroy'])
 ->name('categories.destroy');
+
+// Rutas pay_mode
+// index pay_mode
+Route::get('/pay_mode', [PayModeController::class, 'index'])
+->name('pay_mode.index');
+// store pay_mode
+Route::post('/pay_mode' , [PayModeController::class, 'store'])
+->name('custoemers.store');
+// show pay_mode
+Route::get('/pay_mode/{id}', [PayModeController::class, 'show'])
+->name('pay_mode.show');
+// update pay_mode
+Route::put('/pay_mode/{id}', [PayModeController::class, 'update'])
+->name('pay_mode.update');
+// destroy pay_mode
+Route::delete('/pay_mode/{id}', [PayModeController::class, 'destroy'])
+->name('pay_mode.destroy');
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
