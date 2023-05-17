d63413bd849fe60c16a390dfedf19c38294b7847
May Gaviria <may.dev.app.web@gmail.com>
2023-05-17 01:39:42 -0500

    Categories-index

---
+++ app/Http/Controllers/api/CategoriesController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Sisven;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $categories = DB::table('categories')
+            ->select('categories.*')
+            ->get();
+        return json_encode(['categories' => $categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id)
+    {
+        //
+    }
+}
